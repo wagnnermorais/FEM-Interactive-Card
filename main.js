802c8e47843bb8968def4459d67c3aa9d891c1f0
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-09-11 23:36:42 -0300

    feat: creating form validation

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const form = document.querySelector("#card-form");
+const frontCard = document.querySelector(".front-card");
+const backCard = document.querySelector(".back-card");
+const container = document.querySelector("#container");
 const cardNumber = document.querySelector("#card-number");
 const cardHolder = document.querySelector("#card-holder");
 const cardMonth = document.querySelector("#card-month");
@@ -9,23 +12,92 @@ const formNumber = document.querySelector("#form-card-number");
 const formMonth = document.querySelector("#form-card-month");
 const formYear = document.querySelector("#form-card-year");
 const formCvc = document.querySelector("#form-card-cvc");
+const holderError = document.getElementById("holder-error");
+const numberError = document.getElementById("number-error");
+const monthError = document.getElementById("month-error");
+const yearError = document.getElementById("year-error");
+const cvcError = document.getElementById("cvc-error");
+const showSuccess = document.getElementById("success");
+const backHome = document.getElementsByClassName(".submit-button");
+let isError = false;
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  validForm();
 });
 
 function addValue(input, value) {
-  input.addEventListener("input", (e) => {
-    value.innerText = validateCard(e.target.value);
+  input.addEventListener("input", (event) => {
+    value.innerText = event.target.value;
   });
 }
 
-function validateCard(data) {
-  return data.replace(/^[0-9]{4}[" "][0-9]{4}[" "][0-9]{4}[" "][0-9]{4}/);
+function formatteCardNumber(input, value) {
+  input.addEventListener("input", (event) => {
+    if (
+      input.value.length === 4 ||
+      input.value.length === 9 ||
+      input.value.length === 14
+    ) {
+      input.value += " ";
+    }
+
+    value.innerText = event.target.value;
+  });
+
+  //prettier-ignore
+  if (!input.value.match(/([0-9]{4}\s?)([0-9]{4}\s?)([0-9]{4}\s?)([0-9]{4}\s?)/)) return false;
+  return true;
+}
+
+function setError(input, message) {
+  input.classList.remove("hide");
+  input.innerText = message;
+  isError = true;
+}
+
+function validInput(input) {
+  input.classList.add("hide");
+  isError = false;
+}
+
+function validForm() {
+  formHolder.value.match(
+    /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)\s\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)/gm
+  )
+    ? validInput(holderError)
+    : setError(holderError, "Can't be blank.");
+
+  formatteCardNumber(formNumber, cardNumber)
+    ? validInput(numberError)
+    : setError(numberError, "Wrong format, numbers only.");
+
+  formMonth.value.match(/[0-9]{2}/)
+    ? validInput(monthError)
+    : setError(monthError, "Can't be blank.");
+
+  formYear.value.match(/[0-9]{2}/)
+    ? validInput(yearError)
+    : setError(yearError, "Can't be blank");
+
+  formCvc.value.match(/[0-9]{3}/)
+    ? validInput(cvcError)
+    : setError(cvcError, "Can't be blank");
+
+  if (!isError) {
+    frontCard.classList.add("hide");
+    backCard.classList.add("hide");
+    form.classList.add("hide");
+    showSuccess.classList.remove("hide");
+  }
+}
+
+function backToHome() {
+  console.log("clicou");
 }
 
 addValue(formHolder, cardHolder);
-addValue(formNumber, cardNumber);
+formatteCardNumber(formNumber, cardNumber);
 addValue(formMonth, cardMonth);
 addValue(formYear, cardYear);
 addValue(formCvc, cardCvc);
